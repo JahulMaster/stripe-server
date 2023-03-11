61b8a52451346eea63a54ffd5250cdc18dcdecda
Xki <ugo.linder@gmail.com>
2023-03-11 15:18:04 +0100

    items fix

--- server/server.js
+++ server/server.js
@@ -1,50 +1,52 @@
-require("dotenv").config()
+require('dotenv').config();
 
-const express = require("express")
-const app = express()
-const cors = require("cors")
-app.use(express.json())
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5500",
+    origin: 'http://localhost:5500',
   })
-)
+);
 
-const stripe = require("stripe")(process.env.STRIPE_P_KEY)
+const stripe = require('stripe')(process.env.STRIPE_P_KEY);
 
 const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Learn React Today" }],
-  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
+  [1, { priceInCents: 500, name: '50 Coins' }],
+  [2, { priceInCents: 1000, name: '100 Coins' }],
+  [3, { priceInCents: 2500, name: '250 Coins' }],
+  [4, { priceInCents: 10000, name: '1000 Coins' }],
+]);
 
-app.post("/create-checkout-session", async (req, res) => {
+app.post('/create-checkout-session', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: req.body.items.map(item => {
-        const storeItem = storeItems.get(item.id)
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: req.body.items.map((item) => {
+        const storeItem = storeItems.get(item.id);
         return {
           price_data: {
-            currency: "usd",
+            currency: 'usd',
             product_data: {
               name: storeItem.name,
             },
             unit_amount: storeItem.priceInCents,
           },
           quantity: item.quantity,
-        }
+        };
       }),
       success_url: `${process.env.SERVER_URL}/success.html`,
       cancel_url: `${process.env.SERVER_URL}/cancel.html`,
-    })
-    res.json({ url: session.url })
+    });
+    res.json({ url: session.url });
   } catch (e) {
-    res.status(500).json({ error: e.message })
+    res.status(500).json({ error: e.message });
   }
-})
+});
 
-app.listen(3000)
+app.listen(3000);
 // require('dotenv').config()
 
 // const express= require('express')
@@ -58,7 +60,6 @@ app.listen(3000)
 
 // const stripe = require('stripe')(process.env.STRIPE_P_KEY)
 
-
 // const storeItems = new Map([
 //     [1, { priceInCents: 500,  name: '50 Coins'}],
 //     [2, { priceInCents: 1000, name: '100 Coins'}],
@@ -66,7 +67,7 @@ app.listen(3000)
 //     [4, { priceInCents: 10000, name: '1000 Coins'}],
 // ])
 // app.post("/create-checkout-session", async (req, res) => {
-    
+
 //     const session = await stripe.checkout.sessions.create({
 //     payment_method_types: ['card'],
 //     mode: 'payment',
@@ -90,7 +91,7 @@ app.listen(3000)
 // })
 // app.post('/create-checkout-session', async (req, res)  => {
 //     try{
-         
+
 //         res.status(200).json ({ url: session.url})
 //     }catch (e) {
 //        res.status(500).json({ error: e.message})
